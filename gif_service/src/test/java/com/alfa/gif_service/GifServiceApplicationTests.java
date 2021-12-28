3b5ee796ce44a554b1287288845711f95cc83916
AlexErmakov97 <42092032+AlexErmakov97@users.noreply.github.com>
2021-12-28 20:42:37 +0300

    Update GifServiceApplicationTests.java

--- gif_service/src/test/java/com/alfa/gif_service/GifServiceApplicationTests.java
+++ gif_service/src/test/java/com/alfa/gif_service/GifServiceApplicationTests.java
@@ -1,23 +1,44 @@
 package com.alfa.gif_service;
 
+import com.alfa.gif_service.service.MainService;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
 
-import java.text.SimpleDateFormat;
 import java.time.LocalDate;
-import java.util.Date;
-import java.util.regex.Pattern;
+
+import static ch.qos.logback.core.encoder.ByteArrayUtil.hexStringToByteArray;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class GifServiceApplicationTests {
 
+    @Autowired
+    private MockMvc mvc;
+
+    @MockBean
+    MainService mainService;
+
     @Test
-    void contextLoads() {
+    void mainControllerTest() throws Exception {
+        byte[] testByteArray = hexStringToByteArray("");
+
+        mvc.perform(get("/info/AED")
+                .accept(MediaType.IMAGE_GIF_VALUE))
+                .andExpect(status().is(404))
+                .andExpect(content().bytes(testByteArray));
     }
 
     @Test
-    void formatDate() {
+    void formatDateTest() {
         LocalDate today = LocalDate.now();
         LocalDate yesterday = today.minusDays(1);
         Assertions.assertTrue(today.toString().matches("(20)\\d\\d\\S([1-9]|1[012])\\S([1-9]|[12][0-9]|3[01])"));
